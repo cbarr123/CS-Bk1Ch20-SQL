8c84daff15cf414548621f55fc5a66220e6d1e4d
Carl Barringer <cbarringer1@gmail.com>
2019-10-24 13:44:41 -0700

    now the exercise is complete

--- SQLIntro.sql
+++ SQLIntro.sql
@@ -21,19 +21,19 @@
 --	FROM Song s
 --	LEFT JOIN Artist ar ON s.ArtistId = ar.Id
 --	GROUP BY ar.ArtistName;
-SELECT g.Label, COUNT(s.Title)
-	FROM Song s
-	LEFT JOIN Genre g ON s.GenreId = g.Id
-	GROUP BY g.Label;
+--SELECT g.Label, COUNT(s.Title)
+--	FROM Song s
+--	LEFT JOIN Genre g ON s.GenreId = g.Id
+--	GROUP BY g.Label;
 --SELECT MAX(a.AlbumLength)
 --	FROM Album a;
 SELECT MAX(s.SongLength)
 	FROM Song s;
 
 	-- Just trying something
-SELECT 
-FROM(
-	SELECT a.AlbumLength
+SELECT a.Title, 
+	MaxOne = 
+		(SELECT MAX(a.AlbumLength)
+		FROM Album a)
 	FROM Album a
-	ORDER BY a.AlbumLength DESC
-	);
+	;
